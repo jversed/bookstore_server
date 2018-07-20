a331a21773b5818a2fe23544c70771248fff6606
Alexandr Slobodnichenko <aslobodnichenko@gmail.com>
2018-07-20 21:49:35 +0300

    comment for interface added

--- src/bookstore_server/BookStore.java
+++ src/bookstore_server/BookStore.java
@@ -2,7 +2,9 @@ package bookstore_server;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-
+/*
+ * interface for BookStore same on the client side;
+ */
 public interface BookStore extends Remote{
 	public int giveId() throws RemoteException;
 }
